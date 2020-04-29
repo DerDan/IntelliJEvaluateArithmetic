77898f260ba26da1fc4671de939d9ce86c9b2253
Daniel Franke <franke@edf-online.de>
2020-04-29 09:22:25 +0200

    placeholder "#" and "$" within the numerical expressions added # for the number of selection $ for the last evaluation  result

--- src/main/java/EvaluateArithmeticAction.java
+++ src/main/java/EvaluateArithmeticAction.java
@@ -1,18 +1,23 @@
-import com.intellij.openapi.actionSystem.*;
+import com.fathzer.soft.javaluator.DoubleEvaluator;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.command.WriteCommandAction;
-import com.intellij.openapi.editor.*;
+import com.intellij.openapi.editor.Caret;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
-import com.fathzer.soft.javaluator.DoubleEvaluator;
-
 
 public class EvaluateArithmeticAction extends AnAction {
-    private static DoubleEvaluator evaluator = new DoubleEvaluator();
+    private static final DoubleEvaluator evaluator = new DoubleEvaluator();
+    private String previous_expression_result = "0";
 
     /**
      * Evaluates the selection at each caret as a mathematical expression
      * and replaces the content of the selection with the answer.
-     * @param event  Event related to this action
+     *
+     * @param event Event related to this action
      */
     @Override
     public void actionPerformed(@NotNull final AnActionEvent event) {
@@ -21,14 +26,17 @@ public class EvaluateArithmeticAction extends AnAction {
         final Document document = editor.getDocument();
 
         WriteCommandAction.runWriteCommandAction(project, () -> {
+            previous_expression_result = "0";
+            int index_of_selection = 1;
             for (Caret caret : editor.getCaretModel().getAllCarets()) {
                 String new_text = caret.getSelectedText();
                 if (new_text != null) {
                     document.replaceString(
                             caret.getSelectionStart(),
                             caret.getSelectionEnd(),
-                            evaluate(new_text)
+                            evaluate(new_text, index_of_selection)
                     );
+                    index_of_selection++;
                 }
                 caret.removeSelection();
             }
@@ -37,28 +45,52 @@ public class EvaluateArithmeticAction extends AnAction {
 
     /**
      * Evaluate a given string as an arithmetic expression and return the result as a string
+     *
      * @param expression_string The string to evaluate as an arithmetic expression
      * @return The result of the evaluation if possible or the unchanged string if not
      */
-    static String evaluate(@NotNull String expression_string) {
+    String evaluate(@NotNull String expression_string) {
+        return evaluate(expression_string, 1);
+    }
+
+    /**
+     * Evaluate a given string as an arithmetic expression and return the result as a string
+     *
+     * @param expression_string The string to evaluate as an arithmetic expression
+     * @return The result of the evaluation if possible or the unchanged string if not
+     */
+
+    String evaluate(@NotNull String expression_string, int index_of_selection) {
+        // replace $ with the previous expression result
+        String expression_to_evaluate = expression_string.replaceAll("\\$", previous_expression_result);
+        // replace # with the index of selection
+        expression_to_evaluate = expression_to_evaluate.replaceAll("#", String.valueOf(index_of_selection));
         // Only allow arithmetic characters and whitespace
-        if (expression_string.matches("^[0-9+\\-/*^().\\s]*(=\\s*)?")) {
+        if (expression_to_evaluate.matches("^[0-9+\\-/*^().\\s]*(=\\s*)?")) {
             try {
-                boolean append_result_to_expression = expression_string.contains("=");
+                final boolean append_result_to_expression = expressionToEvaluate.contains("=");
                 // Normalise all whitespace to spaces, remove the optional equal sign (=).
                 // This means groovy expects a single expression
-                String new_string = expression_string.replaceAll("\\s|=", " ");
+                String new_string = expression_to_evaluate.replaceAll("\\s|=", " ");
 
-                String answer = String.valueOf(evaluator.evaluate(new_string));
-                if (answer.contains(".")) {
-                    // Strip all trailing zeroes after decimal point
-                    answer = answer.replaceAll("0*$","");
+                Double value_double = evaluator.evaluate(new_string);
+                long value_long = (long) (value_double * 1d);
+                String answer;
+                if (value_long == value_double) {
+                    answer = String.valueOf(value_long);
+                } else {
+                    answer = String.valueOf(value_double);
+                    if (answer.contains(".")) {
+                        // Strip all trailing zeroes after decimal point
+                        answer = answer.replaceAll("0*$", "");
 
-                    // Remove the decimal point if there's nothing left after it
-                    answer = answer.replaceAll("\\.$","");
+                        // Remove the decimal point if there's nothing left after it
+                        answer = answer.replaceAll("\\.$", "");
 
+                    }
                 }
-                return append_result_to_expression ? (expression_string + answer) : answer;
+                previous_expression_result = answer;
+                return append_result_to_expression ? (expression_to_evaluate + answer) : answer;
             } catch (IllegalArgumentException e) {
                 // The expression was not a valid arithmetic expression
                 return expression_string;
@@ -72,9 +104,9 @@ public class EvaluateArithmeticAction extends AnAction {
         final Project project = event.getProject();
         final Editor editor = event.getData(CommonDataKeys.EDITOR);
         event.getPresentation().setEnabledAndVisible(
-            project != null &&
-            editor != null &&
-            editor.getSelectionModel().hasSelection()
+                project != null &&
+                        editor != null &&
+                        editor.getSelectionModel().hasSelection()
         );
     }
 }
--- src/test/java/ArithmeticTests.java
+++ src/test/java/ArithmeticTests.java
@@ -1,71 +1,136 @@
 import org.junit.Test;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 import static org.junit.Assert.*;
 
 public class ArithmeticTests {
 
     @Test
     public void canDoAllArithmeticOperations() {
-        assertEquals("3", EvaluateArithmeticAction.evaluate("1 + 2"));
-        assertEquals("-1", EvaluateArithmeticAction.evaluate("1 - 2"));
-        assertEquals("6", EvaluateArithmeticAction.evaluate("3 * 2"));
-        assertEquals("2", EvaluateArithmeticAction.evaluate("4 / 2"));
+        EvaluateArithmeticAction action = new EvaluateArithmeticAction();
+        assertEquals("3", action.evaluate("1 + 2"));
+        assertEquals("-1", action.evaluate("1 - 2"));
+        assertEquals("6", action.evaluate("3 * 2"));
+        assertEquals("2", action.evaluate("4 / 2"));
     }
 
     @Test
     public void canOmitWhitespace() {
-        assertEquals("3", EvaluateArithmeticAction.evaluate("1+2"));
-        assertEquals("-1", EvaluateArithmeticAction.evaluate("1-2"));
-        assertEquals("6", EvaluateArithmeticAction.evaluate("3*2"));
-        assertEquals("2", EvaluateArithmeticAction.evaluate("4/2"));
+        EvaluateArithmeticAction action = new EvaluateArithmeticAction();
+        assertEquals("3", action.evaluate("1+2"));
+        assertEquals("-1", action.evaluate("1-2"));
+        assertEquals("6", action.evaluate("3*2"));
+        assertEquals("2", action.evaluate("4/2"));
     }
 
     @Test
     public void canHaveLeadingAndTrailingWhitespace() {
-        assertEquals("3", EvaluateArithmeticAction.evaluate(" 1+2 "));
-        assertEquals("3", EvaluateArithmeticAction.evaluate("      \n      1+2  \n  \t"));
+        EvaluateArithmeticAction action = new EvaluateArithmeticAction();
+        assertEquals("3", action.evaluate(" 1+2 "));
+        assertEquals("3", action.evaluate("      \n      1+2  \n  \t"));
     }
 
     @Test
     public void canHaveInternalNewlines() {
-        assertEquals("3", EvaluateArithmeticAction.evaluate("1+\n2"));
+        EvaluateArithmeticAction action = new EvaluateArithmeticAction();
+        assertEquals("3", action.evaluate("1+\n2"));
     }
 
     @Test
     public void canUseDecimalsInInput() {
-        assertEquals("3", EvaluateArithmeticAction.evaluate("1.5 * 2"));
+        EvaluateArithmeticAction action = new EvaluateArithmeticAction();
+        assertEquals("3", action.evaluate("1.5 * 2"));
     }
 
     @Test
     public void canGetDecimalAnswers() {
+        EvaluateArithmeticAction action = new EvaluateArithmeticAction();
         // We do not guarantee any particular precision, just check that it has at least, say,  5 places
-        assertTrue(EvaluateArithmeticAction.evaluate("2 / 3").startsWith("0.66666"));
+        assertTrue(action.evaluate("2 / 3").startsWith("0.66666"));
     }
 
     @Test
     public void precendenceWorks() {
-        assertEquals("7", EvaluateArithmeticAction.evaluate("1 + 2 * 3"));
-        assertEquals("9", EvaluateArithmeticAction.evaluate("(1 + 2) * 3"));
+        EvaluateArithmeticAction action = new EvaluateArithmeticAction();
+        assertEquals("7", action.evaluate("1 + 2 * 3"));
+        assertEquals("9", action.evaluate("(1 + 2) * 3"));
     }
 
     @Test
+    public void canUsePreviousResult() {
+        EvaluateArithmeticAction action = new EvaluateArithmeticAction();
+        assertEquals("14", action.evaluate("5+9"));
+        assertEquals("14", action.evaluate("$"));
+        assertEquals("15", action.evaluate("$+1"));
+        assertEquals("15+1=16", action.evaluate("$+1="));
+    }
+
+    @Test
+    public void canUseIndexOfSelection() {
+        EvaluateArithmeticAction action = new EvaluateArithmeticAction();
+        assertEquals("0", action.evaluate("# * 2", 0));
+        assertEquals("2", action.evaluate("# * 2", 1));
+        assertEquals("4", action.evaluate("# * 2", 2));
+        assertEquals("4", action.evaluate("$", 2));
+    }
+
+
+    @Test
     public void canAppendResultToExpression() {
-        assertEquals("1 + 1 =2", EvaluateArithmeticAction.evaluate("1 + 1 ="));
+        EvaluateArithmeticAction action = new EvaluateArithmeticAction();
+        assertEquals("1 + 1 =2", action.evaluate("1 + 1 ="));
         // add whitespace after "="
-        assertEquals("5 * 5 = 25", EvaluateArithmeticAction.evaluate("5 * 5 = "));
-        assertEquals("5\t*\t5\t=\n25", EvaluateArithmeticAction.evaluate("5\t*\t5\t=\n"));
+        assertEquals("5 * 5 = 25", action.evaluate("5 * 5 = "));
+        assertEquals("5\t*\t5\t=\n25", action.evaluate("5\t*\t5\t=\n"));
     }
 
     @Test
     public void doesNotEvaluateWrongExpression() {
-        assertEquals("1 + 1 = X", EvaluateArithmeticAction.evaluate("1 + 1 = X"));
-        assertEquals("2 * 2 X", EvaluateArithmeticAction.evaluate("2 * 2 X"));
+        EvaluateArithmeticAction action = new EvaluateArithmeticAction();
+        assertEquals("1 + 1 = X", action.evaluate("1 + 1 = X"));
+        assertEquals("2 * 2 X", action.evaluate("2 * 2 X"));
     }
 
 
     @Test
     public void doesNotEvaluateMultipleExpressions() {
-        assertEquals("1 + 2 3 + 4", EvaluateArithmeticAction.evaluate("1 + 2 3 + 4"));
-        assertEquals("1 + 2\n3 + 4", EvaluateArithmeticAction.evaluate("1 + 2\n3 + 4"));
-        assertEquals("1 + 2 = 3 + 4", EvaluateArithmeticAction.evaluate("1 + 2 = 3 + 4"));
+        EvaluateArithmeticAction action = new EvaluateArithmeticAction();
+        assertEquals("1 + 2 3 + 4", action.evaluate("1 + 2 3 + 4"));
+        assertEquals("1 + 2\n3 + 4", action.evaluate("1 + 2\n3 + 4"));
+        assertEquals("1 + 2 = 3 + 4", action.evaluate("1 + 2 = 3 + 4"));
+    }
+
+    @Test
+    public void canEvaluateBigNaturalNumbers() {
+        EvaluateArithmeticAction action = new EvaluateArithmeticAction();
+        assertEquals("1000000000000001", action.evaluate("1000000000000000+1"));
+        assertEquals("4294967296", action.evaluate("256*256*256*256"));
+        assertEquals("42949672960", action.evaluate("256*256*256*256*10"));
+        assertEquals("4.7244640256E9", action.evaluate("256*256*256*256*1.1"));
+    }
+
+    @Test
+
+    public void canEvaluateHexadecimalNumbers() {
+//        EvaluateArithmeticAction action = new EvaluateArithmeticAction();
+//        assertEquals("257", action.evaluate("0x100+1"));
+        ScriptEngineManager mgr = new ScriptEngineManager();
+        ScriptEngine engine = mgr.getEngineByName("JavaScript");
+        String foo = "40.1+0x2";
+        try {
+            System.out.println(engine.eval(foo));
+        } catch (ScriptException e) {
+            e.printStackTrace();
+        }
+
+//        DoubleEvaluator evaluator = new DoubleEvaluator();
+//        try {
+//            evaluator.evaluate("0x100");
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
     }
 }
